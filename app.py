1b061d3e50eff1281288cc450bf401eb0de94c94
vivi60 <113090754+vivi60@users.noreply.github.com>
2024-12-01 09:21:55 +0900

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 from pydantic import BaseModel
 
-DATABASE_URL = "sqlite:///./community.db"
+DATABASE_URL = "sqlite:////tmp/community.db"
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
