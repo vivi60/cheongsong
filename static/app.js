6b10d411b6e6e018af64a8d9e9aa006f1eab36dd
vivi60 <113090754+vivi60@users.noreply.github.com>
2024-12-01 22:57:51 +0900

    Update app.js

--- static/app.js
+++ static/app.js
@@ -1,4 +1,6 @@
-const API_URL = "https://0.0.0.0:8000"; // FastAPI 서버 주소
+
+const API_URL = "/api"; // 동일한 도메인에서 API를 호출
+
 
 const users = [
     { username: "admin", password: "admin123", role: "admin" },
