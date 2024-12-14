f79f8bdd5868999ddf76342254b15ec884493fe3
vivi60 <113090754+vivi60@users.noreply.github.com>
2024-12-14 16:33:24 +0900

    Update app.js

--- static/app.js
+++ static/app.js
@@ -136,11 +136,9 @@ function renderPosts(posts) {
 
         postElement.innerHTML = `
             <div class="post-header">
-            <div>
-                <h2 class="post-title">${post.title}</h2>\
-                <button class="menu-btn">⋮</button>
+            <h2 class="post-title">${post.title}</h2>
             <div class="menu">
-                
+                <button class="menu-btn">⋮</button>
                 ${canEditDelete(post.author) ? `
                 <div id="menu-${post.id}" class="menu-dropdown hidden">
                     <button onclick="editPost(${post.id})">수정</button>
